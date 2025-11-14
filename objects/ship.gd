d40c6dd0f6fdc9a7e92c8c6273324e9c6ac377c8
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-13 18:55:26 -0800

    basic spaceship control implemented
    enter/exit ship with E, use WASD to steer and Space to thrust

--- objects/ship.gd
+++ objects/ship.gd
@@ -1,11 +1,25 @@
 extends RigidBody3D
-@onready var shipArea := $ShipArea
+@onready var interactArea := $InteractArea
+@export var thrustSpeed:= 0.5
+@export var rotateSpeed :=1.0
 var player
 func _ready() -> void:
 	player = get_parent().get_node("Player")
-	
+	gravity_scale=0.0
+
+func _physics_process(delta: float) -> void:
+	if Global.currentVehicle==self:
+		var thrust_input := Input.get_action_strength("Thrust")
+		var rotate_input := Input.get_vector("left","right","forward","backward")
+		rotation-=Vector3(rotate_input.y,rotate_input.x,0.0)*delta*rotateSpeed
+		rotation.z= lerp_angle(rotation.z,0.0,delta*1.0)
+		linear_velocity+=thrustSpeed*Vector3(0.0,0.0,thrust_input).rotated(self.transform.basis.x,self.rotation.x).rotated(self.transform.basis.y,self.rotation.y).rotated(self.transform.basis.z,self.rotation.z)
+	angular_velocity=angular_velocity.lerp(Vector3.ZERO,delta*1)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Interact"):
+		if not Global.currentVehicle and interactArea.has_overlapping_bodies():
+			Global.currentVehicle=self
 			var posBuffer = player.global_position
 			var rotBuffer = player.global_rotation
 			var player2 = player.duplicate()
@@ -13,3 +27,14 @@ func _unhandled_input(event: InputEvent) -> void:
 			add_child(player2)
 			player2.global_position = posBuffer
 			player2.global_rotation= rotBuffer
+			player=player2
+		elif Global.currentVehicle==self:
+			Global.currentVehicle=null
+			var posBuffer = player.global_position
+			var rotBuffer = player.global_rotation
+			var player2 = player.duplicate()
+			remove_child(player)
+			get_parent().add_child(player2)
+			player2.global_position = posBuffer
+			#player2.global_rotation= rotBuffer
+			player=player2
--- player.gd
+++ player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody3D
-@onready var camera = $Camera3D
+#@onready var camera = $Camera3D
+@onready var camPivot = $SpringArm3D
 @export var move_speed :=10
 @export var mouse_sensitivity:=0.0025
 @export var gravityForce = 10.0
 @export var jumpForce = 10.0
 func _physics_process(delta: float) -> void:
+	if Global.currentVehicle:
+		camPivot.spring_length=lerpf(camPivot.spring_length,20.0,delta*30)
+	else:
+		camPivot.spring_length=lerpf(camPivot.spring_length,0.0,delta*30)
 	var input_dir = Input.get_vector("left","right","forward","backward")
-	input_dir = input_dir.rotated(-camera.rotation.y)
+	input_dir = input_dir.rotated(-camPivot.rotation.y)
+	if Global.currentVehicle:
+		input_dir=Vector2.ZERO
 	velocity.x=input_dir.x*move_speed
 	velocity.z=input_dir.y*move_speed
 	if is_on_floor():
@@ -21,8 +28,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("unlock mouse"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		camera.rotation.y-=event.relative.x*mouse_sensitivity
-		camera.rotation.x-=event.relative.y*mouse_sensitivity
-		camera.rotation.x= clampf(camera.rotation.x,-PI/2,PI/2)
-	if event.is_action_pressed("jump"):
+		camPivot.rotation.y-=event.relative.x*mouse_sensitivity
+		camPivot.rotation.x-=event.relative.y*mouse_sensitivity
+		camPivot.rotation.x= clampf(camPivot.rotation.x,-PI/2,PI/2)
+	if event.is_action_pressed("jump") and not Global.currentVehicle:
 		velocity.y=jumpForce
