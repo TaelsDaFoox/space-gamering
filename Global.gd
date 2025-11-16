e1a4b1364ce0e03f12b63fb6f09d9911408d5682
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-16 04:44:40 -0800

    npc now care about what package you have
    unique dialogue for all combinations of having a package and the different NPCs, also slightly brighter ambient light

--- Global.gd
+++ Global.gd
@@ -5,3 +5,4 @@ var targetPos := Vector3.ZERO
 var homePos :=Vector3(-6.0,1.0,18.0)
 var stations = []
 var deliveryDialogue:=["Could you deliver this to Station [S] for me?","I have a package that needs to be delivered to Station [S].","Please, deliver this to Station [S]!","I gotta mail this to Station [S], would you mind delivering it?","Deliver this to Station [S], would ya?", "This was sent here, but the address says it was supposed to go to Station [S]... would you mind shipping it there?"]
+var targetStation
--- objects/npc.gd
+++ objects/npc.gd
@@ -9,13 +9,24 @@ func _unhandled_input(event: InputEvent) -> void:
 			Global.currentVehicle=self
 			var spawn = textbox.instantiate()
 			if npcType == "Sender":
-				var selectedStation = Global.stations[randi_range(0,Global.stations.size()-1)]
-				Global.targetPos=selectedStation.npc.global_position
-				spawn.textqueue = Global.deliveryDialogue[randi_range(0,Global.deliveryDialogue.size()-1)].replacen("[S]",selectedStation.stationName)
+				if not Global.targetStation:
+					var selectedStation = Global.stations[randi_range(0,Global.stations.size()-1)]
+					Global.targetPos=selectedStation.npc.global_position
+					Global.targetStation = selectedStation
+					spawn.textqueue = Global.deliveryDialogue[randi_range(0,Global.deliveryDialogue.size()-1)].replacen("[S]",selectedStation.stationName)
+				else:
+					spawn.textqueue="Hey, deliver that package you're holding before you take another one!"
 				spawn.header = "Rhea 2 (the sequel)"
 			if npcType == "Recipient":
-				spawn.textqueue = "am recipient yes hello hii hiiii hello hiiiii"
-				Global.targetPos=Global.homePos
+				if Global.targetStation and get_parent() == Global.targetStation:
+					spawn.textqueue = "am recipient yes hello hii hiiii hello hiiiii"
+					Global.targetPos=Global.homePos
+					Global.targetStation=null
+				else:
+					if Global.targetStation:
+						spawn.textqueue = "Huh? No, this package isn't mine... It's addressed to Station [S].".replacen("[S]",Global.targetStation.stationName)
+					else:
+						spawn.textqueue = "Hm? No, I don't have any packages to deliver..."
 			get_parent().add_child(spawn)
 		elif Global.currentVehicle==self:
 			get_parent().get_node("TextboxUI").queue_free()
