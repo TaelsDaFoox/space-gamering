771bb775b5761f7bcb75fe84ddfcb004c53e12d0
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-16 18:18:57 -0800

    added music and sfx
    music is Lost Galaxy from Bloxels, and there's some stock sound I've been using forever as the thruster sfx

--- objects/ship.gd
+++ objects/ship.gd
@@ -4,6 +4,7 @@ extends RigidBody3D
 @export var rotateSpeed :=2.0
 @onready var fire := $ThrusterFire
 @onready var firetimer = $ThrusterFire/Timer
+@onready var thrustSFX = $ThrusterSound
 var player
 func _ready() -> void:
 	player = get_parent().get_node("Player")
@@ -18,13 +19,17 @@ func _physics_process(delta: float) -> void:
 		var thrust_input := Input.get_action_strength("Thrust")
 		if thrust_input:
 			fire.visible=true
+			if not thrustSFX.playing:
+				thrustSFX.playing=true
 		else:
 			fire.visible=false
+			thrustSFX.playing=false
 		var rotate_input := Input.get_vector("left","right","forward","backward")
 		rotation-=Vector3(rotate_input.y,rotate_input.x,0.0)*delta*rotateSpeed
 		linear_velocity+=thrustSpeed*Vector3(0.0,0.0,thrust_input).rotated(self.transform.basis.x,self.rotation.x).rotated(self.transform.basis.y,self.rotation.y).rotated(self.transform.basis.z,self.rotation.z)
 	else:
 		fire.visible=false
+		thrustSFX.playing=false
 		gravity_scale=1.0
 	rotation.z= lerp_angle(rotation.z,0.0,delta)
 	angular_velocity=angular_velocity.lerp(Vector3.ZERO,delta*1)
